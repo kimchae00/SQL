2f97ebd03f03c7b618ddb151ed575465d6c44a0a
kcy013100 <kcy013100@naver.com>
2022-08-25 13:18:15 +0900

    2022/08/24 추가

---
+++ 제3장 데이터베이스 관리.sql
@@ -0,0 +1,11 @@
+#날짜 : 2022/08/24
+#이름 : 김채영
+#내용 : 제3장 데이터베이스 관리
+
+#실습3-1
+create database `java1_test_db`;
+create user 'java1_test'@'%' identified by '1234';
+grant all privileges on `java1_test_db`.* to 'java1_test'@'%';
+flush privileges;
+
+#실습3-2
